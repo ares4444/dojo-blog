2208f91219ba7a5c8b224da0b6c5a7e5df9de772
Moises De Leon <mdeleon5826@gmail.com>
2022-04-09 12:02:13 -0400

    useState

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,20 @@
+import { useState } from 'react';
+
 const Home = () => {
+    // let name = "Mario";
+    const  [name, setName] = useState('Mario');
+    const  [age, setAge] = useState(25);
 
     const handleClick = () => {
-        console.log("Hello")
-    }
-
-    const handleClickAgain = (name) => {
-        console.log('hello ' + name)
+        setName('Luigi');
+        setAge(50)
     }
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
-            <button onClick={{ handleClick }}>Click Me</button>
-            <button onClick={() => 
-                handleClickAgain("Mario")
-            }>Click Me Again</button>
+            <p>{ name } is { age } years old!</p>
+            <button onClick={ handleClick }>Click Me</button>
         </div>
      );
 }
