bbdb5e9955364a443aa726b3fe956930edd5cdc8
Moises De Leon <mdeleon5826@gmail.com>
2022-04-09 11:32:42 -0400

    added initial components for home and nav

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import './App.css';
+import Navbar from './Navbar'
+import Home from './Home'
 
 function App() {
-  const title = 'Welcome to te New Blog'
-  const likes = 50;
-
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Likes { likes } times</p>
-        <p>{ 10 }</p>
-        <p>{ "Hello World" }</p>
-        <p>{ [1,2,3,4,5] }</p>
-        <p>{ Math.random() * 20 }</p>
-
+        <Home />
       </div>
     </div>
   );
