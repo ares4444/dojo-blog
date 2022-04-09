849994f4aa059e8d721cb4dfec0b50af0db13b27
Moises De Leon <mdeleon5826@gmail.com>
2022-04-09 11:52:06 -0400

    handleClick and added CSS

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log("Hello")
+    }
+
+    const handleClickAgain = (name) => {
+        console.log('hello ' + name)
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={{ handleClick }}>Click Me</button>
+            <button onClick={() => 
+                handleClickAgain("Mario")
+            }>Click Me Again</button>
         </div>
      );
 }
