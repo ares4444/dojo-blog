fc57128578aad30f83a3c0f7a8cda71456ba2d34
Moises De Leon <mdeleon5826@gmail.com>
2022-04-10 15:46:20 -0400

    dreated data file to hold our data. finished the fetch and created some loading time

--- src/Home.js
+++ src/Home.js
@@ -2,29 +2,27 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
-
-  const [name, setName] = useState('Mario')
-
-  const handleDelete = (id) => {
-      const newBlogs = blogs.filter(blog => blog.id !== id);
-      setBlogs(newBlogs);
-  }
+  const [blogs, setBlogs] = useState(null)
+  const [isPending, setIsPending] = useState(true)
 
   useEffect(() => {
-    console.log(name);
-    console.log('use effect ran');
-  }, [name])
+    setTimeout(() => {
+        fetch('http://localhost:8000/blogs')
+    .then(res => {
+        return res.json()
+    })
+    .then(data => {
+        console.log(data);
+        setBlogs(data)
+        setIsPending(false)
+    })
+    }, 1000)
+  }, [])
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
-      <button onClick={() => setName('Luigi')}>Change Name</button>
-      <p>{ name }</p>
+        { isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
   );
 }
